834587305b19d1699650a80226bf14b6265bde87
nadya <nadiasantikanaraya@gmail.com>
2024-10-04 10:05:13 +0700

    Add files via upload

---
+++ Latihan22.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan22
+ */
+public class Latihan22 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Nadya Syantika naraya");
+        System.out.println("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Tugas22.java
@@ -0,0 +1,19 @@
+/**
+ * Tugas 22
+ */
+public class Tugas22 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Nadya Syantika Naraya");
+        System.out.println("NIM     : 244107060147");
+        System.out.println("Kelas   : 1F");
+        System.out.println("No Absen: 22");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Jalan Laksda Adi Sucipto 22A");
+        System.out.println("No HP   : 089515320507");
+        System.out.println("Hobi    : Nonton");
+    }
+}
